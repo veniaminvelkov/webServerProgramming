5a1a04c4e2cbbc71feeca0a7a19498c17580d02a
Veniamin <47269098+veniaminvelkov@users.noreply.github.com>
2025-09-08 17:43:28 +0300

    Create CreateGarageDTO
    Signed-off-by: Veniamin <47269098+veniaminvelkov@users.noreply.github.com>

---
+++ src/main/java/org/springboottest/demo/dtos/CreateGarageDTO.java
@@ -0,0 +1,29 @@
+package org.springboottest.demo.dtos;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateGarageDTO {
+
+    //NotBlank - NotEmpty, but removes spaces
+    //NotEmpty - NotNull, but also checks empty strings ("")
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String location;
+
+    @NotBlank
+    private String city;
+
+    @NotNull
+    @Min(0)
+    private int capacity;
+}
