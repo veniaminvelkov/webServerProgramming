997e3f72c4f15fb55344c161e4d2b9f0bab32efc
Veniamin <47269098+veniaminvelkov@users.noreply.github.com>
2025-09-08 19:17:58 +0300

    Create basic GarageController for testing
    Signed-off-by: Veniamin <47269098+veniaminvelkov@users.noreply.github.com>

---
+++ src/main/java/org/springboottest/demo/controllers/GarageController.java
@@ -0,0 +1,36 @@
+package org.springboottest.demo.controllers;
+
+import jakarta.validation.Valid;
+import org.springboottest.demo.dtos.CreateGarageDTO;
+import org.springboottest.demo.dtos.ResponseGarageDTO;
+import org.springboottest.demo.services.GarageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/garages")
+public class GarageController {
+
+    private GarageService garageService;
+
+    public GarageController(GarageService garageService) {
+        this.garageService = garageService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseGarageDTO>> getGarage() {
+        return  ResponseEntity.ok(garageService.getAllGarages());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseGarageDTO> getGarageById(@PathVariable Long id) {
+        return ResponseEntity.ok(garageService.getGarageById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseGarageDTO> createGarage(@Valid @RequestBody CreateGarageDTO dto) {
+        return ResponseEntity.ok(garageService.createGarage(dto));
+    }
+}
